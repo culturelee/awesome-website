77f806359fd3370fdf6299bda6033d38c2fbf1c1
culturelee <culturelee@163.com>
2019-07-17 09:08:33 +0800

    提交app。py

---
+++ www/app.py
@@ -0,0 +1,14 @@
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio
+from aiohttp import web
+
+#定义服务器响应请求的返回为“Awesome Website”
+async def index(request):
+	return web.Response(body=b'<h1>Awesome Website<h1>',content_type='text/html')
+
+#建立服务器应用，持续监听本地9000端口的http请求，对首页“/”进行响应
+def init():
+	app = web.Application()
+	
+
+
